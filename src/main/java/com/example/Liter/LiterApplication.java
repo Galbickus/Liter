81d569bb902c8fa80b8dc39d58eea3329f5a7d1a
Veronica <vgalbicka@gmail.com>
2024-05-23 00:17:38 -0300

    Initial commit

---
+++ src/main/java/com/example/Liter/LiterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Liter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterApplication.class, args);
+	}
+
+}
