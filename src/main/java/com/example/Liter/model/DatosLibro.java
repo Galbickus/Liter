d3b15d185cbec3c30aed2e93cb9f4a8300f4e501
Veronica <vgalbicka@gmail.com>
2024-05-24 04:15:05 -0300

    Update DatosLibro.java

--- src/main/java/com/example/Liter/model/DatosLibro.java
+++ src/main/java/com/example/Liter/model/DatosLibro.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosLibro(@JsonAlias("title") String titulo,
-                         @JsonAlias("authors") String autor,
-                         @JsonAlias("languages") String idioma,
-                         @JsonAlias("copyright") String copyright,
-                         @JsonAlias("download_count") Integer descargas) {
+public record DatosLibro(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") String autor,
+        @JsonAlias("languages") String idioma,
+        @JsonAlias("copyright") String copyright,
+        @JsonAlias("download_count") Integer descargas) {
 }
 
 /*@JsonAlias solo lee, @JsonProperty se puede leer y escribir para adicionar datos en una api*/
