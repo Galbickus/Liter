cefdaa8c9cbe89919c091978783bec3277429358
Veronica <vgalbicka@gmail.com>
2024-05-24 04:30:05 -0300

    Create Idioma.java

---
+++ src/main/java/com/example/Liter/model/Idioma.java
@@ -0,0 +1,31 @@
+package com.example.Liter.model;
+
+public enum Idioma {
+    ES("es"),
+    FR("fr"),
+    EN("en"),
+    PT("pt");
+
+    private String idioma;
+
+    Idioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getIdioma(){
+        return this.idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public static Idioma fromString(String text) {
+        for (Idioma idioma : Idioma.values()) {
+            if (idioma.idioma.equalsIgnoreCase(text)) {
+                return idioma;
+            }
+        }
+        throw new IllegalArgumentException("No constant with text " + text + " found");
+    }
+}
