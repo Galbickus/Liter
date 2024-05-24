60d6e58d468130a89f5d563b3878df09126c9392
Veronica <vgalbicka@gmail.com>
2024-05-24 03:32:32 -0300

    Create ConvierteDatos.java

---
+++ src/main/java/com/example/Liter/service/ConvierteDatos.java
@@ -0,0 +1,9 @@
+package com.example.Liter.service;
+
+import com.example.Liter.model.DatosLibro;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvierteDatos {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+}
