e539c8b40ac5d382eb6eda0f2afb25667197c76e
Veronica <vgalbicka@gmail.com>
2024-05-24 01:52:41 -0300

    IMPLEMENTA  Y SOBREESCRIBE
    IMPLEMENTA CommandLineRunner y SOBREESCRIBE su método run()

--- src/main/java/com/example/Liter/LiterApplication.java
+++ src/main/java/com/example/Liter/LiterApplication.java
@@ -1,13 +1,18 @@
 package com.example.Liter;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterApplication {
+public class LiterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
