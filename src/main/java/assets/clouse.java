5909f40080e13f9e49bcaaa81b484b57d165c7c3
Veronica <vgalbicka@gmail.com>
2024-06-14 22:18:37 -0300

    Create clouse.java

---
+++ src/main/java/assets/clouse.java
@@ -0,0 +1,17 @@
+package assets;
+
+public class clouse {
+    private int number;
+
+    public clouse(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
